7c71dd6a12149bfbe1df733d6b0f62818a7f14c3
Itai Hay <3392524+itaihay@users.noreply.github.com>
2021-07-15 14:58:32 +0300

    Finished adding data transformers

--- complex_gan_dataset_2.py
+++ complex_gan_dataset_2.py
@@ -8,9 +8,18 @@ from complex_gan import ComplexGAN
 data = arff.loadarff("german_credit.arff")
 data = pd.DataFrame(data[0])
 
-data = data.copy().reset_index(drop=True)
-data = pd.get_dummies(data)
-scaled_data = PowerTransformer(method='yeo-johnson', standardize=True, copy=True).fit_transform(data)
+scaled_data = data.copy().reset_index(drop=True)
+scaled_data = pd.get_dummies(scaled_data, prefix_sep='class_col', drop_first=True)
+class_col = [col for col in scaled_data.columns if 'class_col' in col]
+num_col = [col for col in scaled_data.columns if col not in class_col]
+binary_col = [col for col in num_col if len(scaled_data[col].unique()) == 2]
+
+# Remove all the binary columns
+[num_col.remove(col) for col in binary_col]
+
+data_transformer = PowerTransformer(method='yeo-johnson', standardize=True, copy=True)
+scaled_data[num_col] = data_transformer.fit_transform(scaled_data[num_col])
+scaled_data = pd.get_dummies(scaled_data, prefix="binary", columns=binary_col, drop_first=True)
 
 # Parameters
 noise_dim = 32
@@ -22,7 +31,10 @@ learning_rate = 1e-5
 models_dir = 'weight_cache_complex_dataset2'
 
 # Training the GAN
-complex_gan = ComplexGAN(batch_size=batch_size, learning_rate=learning_rate, noise_dim=noise_dim, data_shape=data.shape,
+complex_gan = ComplexGAN(batch_size=batch_size,
+                         learning_rate=learning_rate,
+                         noise_dim=noise_dim,
+                         data_shape=scaled_data.shape,
                          layers_dim=layer_dim)
 complex_gan.train(scaled_data, epochs)
 
@@ -31,8 +43,10 @@ gen_model = complex_gan.generator
 noise = np.random.normal(size=(100, noise_dim))
 c_gen = np.random.uniform(0, 1, size=(100, 1))
 
-df_generated_data = pd.DataFrame(complex_gan.generator.predict((noise, c_gen)), columns=data.columns)
+df_generated_data = pd.DataFrame(complex_gan.generator.predict((noise, c_gen)), columns=scaled_data.columns)
 bb_probabilities = complex_gan.black_box_model.predict_proba(
     df_generated_data.iloc[:, 0:df_generated_data.shape[1] - 1])[:, 1]
 predict_generated_data = complex_gan.discriminator.predict([df_generated_data, c_gen, bb_probabilities])
 print(f"We faked {sum(predict_generated_data > 0.5)} out of 100")
+df_generated_data[num_col] = data_transformer.inverse_transform(df_generated_data[num_col])
+print(df_generated_data.head(20))
--- simple_gan_dataset_2.py
+++ simple_gan_dataset_2.py
@@ -8,10 +8,18 @@ from simplegan import SimpleGan
 data = arff.loadarff("german_credit.arff")
 data = pd.DataFrame(data[0])
 
-data = data.copy().reset_index(drop=True)
-data = pd.get_dummies(data)
-scaled_data = PowerTransformer(method='yeo-johnson', standardize=True, copy=True).fit_transform(data)
-scaled_data = pd.DataFrame(scaled_data, columns=data.columns)
+scaled_data = data.copy().reset_index(drop=True)
+scaled_data = pd.get_dummies(scaled_data, prefix_sep='class_col', drop_first=True)
+class_col = [col for col in scaled_data.columns if 'class_col' in col]
+num_col = [col for col in scaled_data.columns if col not in class_col]
+binary_col = [col for col in num_col if len(scaled_data[col].unique()) == 2]
+
+# Remove all the binary columns
+[num_col.remove(col) for col in binary_col]
+
+data_transformer = PowerTransformer(method='yeo-johnson', standardize=True, copy=True)
+scaled_data[num_col] = data_transformer.fit_transform(scaled_data[num_col])
+scaled_data = pd.get_dummies(scaled_data, prefix="binary", columns=binary_col, drop_first=True)
 
 # Define the GAN and training parameters
 noise_dim = 64
@@ -20,15 +28,20 @@ batch_size = 32
 log_step = 100
 epochs = 1000 + 1
 learning_rate = 5e-4
-models_dir = 'weight_cache_simple_dataset2'
+models_dir = 'weight_cache_simple_dataset1'
 
 # Training the GAN model chosen: Vanilla GAN, CGAN, DCGAN, etc.
-simple_gan = SimpleGan(batch_size=batch_size, learning_rate=learning_rate, noise_dim=noise_dim, data_shape=data.shape,
-                       layers_dim=layer_dim)
+simple_gan = SimpleGan(batch_size=batch_size,
+                         learning_rate=learning_rate,
+                         noise_dim=noise_dim,
+                         data_shape=scaled_data.shape,
+                         layers_dim=layer_dim)
 simple_gan.train(scaled_data, epochs)
 
 gen_model = simple_gan.generator
 df_generated_data = pd.DataFrame(simple_gan.generator.predict(np.random.normal(size=(1000, noise_dim))),
-                                 columns=data.columns)
+                                 columns=scaled_data.columns)
 predict_generated_data = simple_gan.discriminator.predict([df_generated_data])
 print(f"We faked {sum(predict_generated_data > 0.5)} out of 1000")
+df_generated_data[num_col] = data_transformer.inverse_transform(df_generated_data[num_col])
+print(df_generated_data.head(20))
