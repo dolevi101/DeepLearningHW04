a613d80853317454100858a9e074d81bb190573c
Dolev <dolevi101@gmail.com>
2021-07-15 08:28:11 +0300

    trying to fix problem with dataset 2

--- gan_part_1.py
+++ gan_part_1.py
@@ -1,4 +1,21 @@
 import tensorflow as tf
+from scipy.io import arff
+import pandas as pd
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.preprocessing import StandardScaler
+
+
+def load_dataset(path):
+    data, meta = arff.loadarff(path)
+    df = pd.DataFrame(data)
+    return df
+
+
 if __name__ == "__main__":
-    print("hello")
-    tf.config.list_physical_devices('GPU')
+    for dataset_path in ['german_credit.arff', 'diabetes.arff']:
+        df = load_dataset(dataset_path)
+        nRow, nCol = df.shape
+        #print(f'There are {nRow} rows and {nCol} columns in the {dataset_path} dataset')
+        #print(df.info())
+        print(df.nunique())
+
